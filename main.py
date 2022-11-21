426ecef47594c1c4e839400d96c24f0eeaaf8f0f
engenx <mckennj2@gmail.com>
2022-11-21 14:07:22 -0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from turtle import Screen, Turtle
-import Paddle
+from paddle import Paddle
 
 screen = Screen()
 screen.setup(width=800, height=600)
@@ -7,30 +7,21 @@ screen.bgcolor("black")
 screen.title("Turtle Ping Pong")
 screen.tracer(0)
 
+
 rpad = Paddle((350, 0))
 lpad = Paddle((-350, 0))
 
 
-def go_up():
-    new_y = rpad.ycor() + 20
-    rpad.goto(rpad.xcor(), new_y)
-
-
-def go_down():
-    new_y = rpad.ycor() - 20
-    rpad.goto(rpad.xcor(), new_y)
-
-
 screen.listen()
-screen.onkey(go_up, "Up")
-screen.onkey(go_down, "Down")
+screen.onkey(rpad.go_up, "Up")
+screen.onkey(rpad.go_down, "Down")
+screen.onkey(lpad.go_up, "w")
+screen.onkey(lpad.go_down, "s")
 
 game_is_on = True
 while game_is_on:
     screen.update()
 
 
-
-
-
 screen.exitonclick()
+
